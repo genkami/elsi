322941403a5c793dca13f491a662288605b1b5b8
Genta Kamitani <oftn.mofumofu@gmail.com>
2023-07-01 13:54:47 +0900

    add message.{Uint,Int}{8,16,32}

---
+++ elrpc/message/primitive.go
@@ -0,0 +1,262 @@
+package message
+
+type Void struct{}
+
+func (Void) UnmarshalELRPC(dec *Decoder) error {
+	return nil
+}
+
+func (Void) MarshalELRPC(enc *Encoder) error {
+	return nil
+}
+
+func (Void) ZeroMessage() Message {
+	return Void{}
+}
+
+type Uint8 struct {
+	Value uint8
+}
+
+var _ Message = (*Uint8)(nil)
+
+func (u *Uint8) UnmarshalELRPC(dec *Decoder) error {
+	val, err := dec.DecodeUint8()
+	if err != nil {
+		return err
+	}
+	u.Value = val
+	return nil
+}
+
+func (u *Uint8) MarshalELRPC(enc *Encoder) error {
+	return enc.EncodeUint8(u.Value)
+}
+
+func (u *Uint8) ZeroMessage() Message {
+	return &Uint8{}
+}
+
+type Uint16 struct {
+	Value uint16
+}
+
+var _ Message = (*Uint16)(nil)
+
+func (u *Uint16) UnmarshalELRPC(dec *Decoder) error {
+	val, err := dec.DecodeUint16()
+	if err != nil {
+		return err
+	}
+	u.Value = val
+	return nil
+}
+
+func (u *Uint16) MarshalELRPC(enc *Encoder) error {
+	return enc.EncodeUint16(u.Value)
+}
+
+func (u *Uint16) ZeroMessage() Message {
+	return &Uint16{}
+}
+
+type Uint32 struct {
+	Value uint32
+}
+
+var _ Message = (*Uint32)(nil)
+
+func (u *Uint32) UnmarshalELRPC(dec *Decoder) error {
+	val, err := dec.DecodeUint32()
+	if err != nil {
+		return err
+	}
+	u.Value = val
+	return nil
+}
+
+func (u *Uint32) MarshalELRPC(enc *Encoder) error {
+	return enc.EncodeUint32(u.Value)
+}
+
+func (u *Uint32) ZeroMessage() Message {
+	return &Uint32{}
+}
+
+type Uint64 struct {
+	Value uint64
+}
+
+var _ Message = (*Uint64)(nil)
+
+func (u *Uint64) UnmarshalELRPC(dec *Decoder) error {
+	val, err := dec.DecodeUint64()
+	if err != nil {
+		return err
+	}
+	u.Value = val
+	return nil
+}
+
+func (u *Uint64) MarshalELRPC(enc *Encoder) error {
+	return enc.EncodeUint64(u.Value)
+}
+
+func (u *Uint64) ZeroMessage() Message {
+	return &Uint64{}
+}
+
+type Int8 struct {
+	Value int8
+}
+
+var _ Message = (*Int8)(nil)
+
+func (i *Int8) UnmarshalELRPC(dec *Decoder) error {
+	val, err := dec.DecodeInt8()
+	if err != nil {
+		return err
+	}
+	i.Value = val
+	return nil
+}
+
+func (i *Int8) MarshalELRPC(enc *Encoder) error {
+	return enc.EncodeInt8(i.Value)
+}
+
+func (i *Int8) ZeroMessage() Message {
+	return &Int8{}
+}
+
+type Int16 struct {
+	Value int16
+}
+
+var _ Message = (*Int16)(nil)
+
+func (i *Int16) UnmarshalELRPC(dec *Decoder) error {
+	val, err := dec.DecodeInt16()
+	if err != nil {
+		return err
+	}
+	i.Value = val
+	return nil
+}
+
+func (i *Int16) MarshalELRPC(enc *Encoder) error {
+	return enc.EncodeInt16(i.Value)
+}
+
+func (i *Int16) ZeroMessage() Message {
+	return &Int16{}
+}
+
+type Int32 struct {
+	Value int32
+}
+
+var _ Message = (*Int32)(nil)
+
+func (i *Int32) UnmarshalELRPC(dec *Decoder) error {
+	val, err := dec.DecodeInt32()
+	if err != nil {
+		return err
+	}
+	i.Value = val
+	return nil
+}
+
+func (i *Int32) MarshalELRPC(enc *Encoder) error {
+	return enc.EncodeInt32(i.Value)
+}
+
+func (i *Int32) ZeroMessage() Message {
+	return &Int32{}
+}
+
+type Int64 struct {
+	Value int64
+}
+
+var _ Message = (*Int64)(nil)
+
+func (i *Int64) UnmarshalELRPC(dec *Decoder) error {
+	val, err := dec.DecodeInt64()
+	if err != nil {
+		return err
+	}
+	i.Value = val
+	return nil
+}
+
+func (i *Int64) MarshalELRPC(enc *Encoder) error {
+	return enc.EncodeInt64(i.Value)
+}
+
+func (i *Int64) ZeroMessage() Message {
+	return &Int64{}
+}
+
+type Bytes struct {
+	Value []byte
+}
+
+func (b *Bytes) UnmarshalELRPC(dec *Decoder) error {
+	val, err := dec.DecodeBytes()
+	if err != nil {
+		return err
+	}
+	b.Value = val
+	return nil
+}
+
+func (b *Bytes) MarshalELRPC(enc *Encoder) error {
+	return enc.EncodeBytes(b.Value)
+}
+
+func (b *Bytes) ZeroMessage() Message {
+	return &Bytes{}
+}
+
+type String struct {
+	Value string
+}
+
+func (s *String) UnmarshalELRPC(dec *Decoder) error {
+	val, err := dec.DecodeString()
+	if err != nil {
+		return err
+	}
+	s.Value = val
+	return nil
+}
+
+func (s *String) MarshalELRPC(enc *Encoder) error {
+	return enc.EncodeString(s.Value)
+}
+
+func (s *String) ZeroMessage() Message {
+	return &String{}
+}
+
+type Any struct {
+	Raw []byte
+}
+
+func (a *Any) UnmarshalELRPC(dec *Decoder) error {
+	b, err := dec.DecodeAny()
+	if err != nil {
+		return err
+	}
+	a.Raw = b.Raw
+	return nil
+}
+
+func (a *Any) MarshalELRPC(enc *Encoder) error {
+	return enc.EncodeAny(a)
+}
+
+func (a *Any) ZeroMessage() Message {
+	return &Any{}
+}
